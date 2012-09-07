6254e345394b3bb67cec1b60e7796eb677d2d22a
Song Zheng <coldangelfire311@gmail.com>
2012-09-07 14:57:15 -0700

    close #4 call deleteArchive( archiveId, token ) to delete archives

--- spec/opentok_spec.rb
+++ spec/opentok_spec.rb
@@ -107,6 +107,21 @@ describe OpenTok do
     end
   end
 
+  describe "Delete Archives" do
+    use_vcr_cassette "deleteArchive"
+    let(:api_key) { '459782' }
+    let(:api_secret) { 'b44c3baa32b6476d9d88e8194d0eb1c6b777f76b' }
+    let(:opentok) { OpenTok::OpenTokSDK.new api_key, api_secret, {:api_url=>""} }
+    let(:session) { '1_MX40NTk3ODJ-MTI3LjAuMC4xflR1ZSBTZXAgMDQgMTQ6NTM6MDIgUERUIDIwMTJ-MC41MjExODEzfg' }
+    let(:token) { opentok.generateToken({:session_id => session, :role=>OpenTok::RoleConstants::PUBLISHER}) }
+    let(:archiveId) { "200567af-0726-4e93-883b-fe0426d6310a" }
+
+    it "should return false on wrong moderator" do
+      a = opentok.deleteArchive( archiveId, token )
+      a.should eq false
+    end
+  end
+
   describe "stitch api" do
     use_vcr_cassette "stitchArchive"
     let(:api_key) { '459782' }
