58f0dd1be763508d1cf0deec0cf772f6545d65e3
Jeff Swartz <swartz@tokbox.com>
2014-05-15 17:29:27 -0700

    Partial fix for https://github.com/opentok/Opentok-Ruby-SDK/issues/53

--- lib/opentok/archive.rb
+++ lib/opentok/archive.rb
@@ -3,6 +3,49 @@ require "active_support/inflector"
 
 module OpenTok
     # Represents an archive of an OpenTok session.
+    #
+    # @attr [int] createdAt
+    #   The time at which the archive was created, in milliseconds since the UNIX epoch.
+    #
+    # @attr [string] duration
+    #   The duration of the archive, in milliseconds.
+    #
+    # @attr [string] id
+    #   The archive ID.
+    #
+    # @attr [string] name
+    #   The name of the archive. If no name was provided when the archive was created, this is set
+    #   to null.
+    #
+    # @attr [string] partnerId
+    #   The API key associated with the archive.
+    #
+    # @attr [string] reason
+    #   For archives with the status "stopped", this can be set to "90 mins exceeded", "failure",
+    #   "session ended", or "user initiated". For archives with the status "failed", this can be set
+    #   to "system failure".
+    #
+    # @attr [string] sessionId
+    #   The session ID of the OpenTok session associated with this archive.
+    #
+    # @attr [float] size
+    #   The size of the MP4 file. For archives that have not been generated, this value is set to 0.
+    #
+    # @attr [string] status
+    #   The status of the archive, which can be one of the following:
+    #
+    #   * "available" -- The archive is available for download from the OpenTok cloud.
+    #   * "failed" -- The archive recording failed.
+    #   * "started" -- The archive started and is in the process of being recorded.
+    #   * "stopped" -- The archive stopped recording.
+    #   * "uploaded" -- The archive is available for download from the the upload target
+    #     S3 bucket.
+    #
+    # @attr [string] url
+    #   The download URL of the available MP4 file. This is only set for an archive with the status set to
+    #   "available"; for other archives, (including archives with the status "uploaded") this property is
+    #   set to null. The download URL is obfuscated, and the file is only available from the URL for
+    #   10 minutes. To generate a new URL, call the Archive.listArchives() or OpenTok.getArchive() method.
     class Archive
 
     # @private
@@ -31,7 +74,7 @@ module OpenTok
       @json = @interface.delete_by_id @json['id']
     end
 
-    # @private
+    # @private ignore
     def method_missing(method, *args, &block)
       camelized_method = method.to_s.camelize(:lower)
       if @json.has_key? camelized_method and args.empty?
