be76801c6fbc7e0c5e9f8065e6282e2952ae5eea
Aiham Hammami <aiham@aiham.net>
2017-06-09 10:16:49 +1000

    Update JS samples to use latest API and add error handling (#144)
    * Update JS samples to use latest API and add error handling
    
    * Update JS style: single quotes
    
    * Update URL for JS SDK
    
    * Return the initPublisher callback in helloworld sample

--- sample/HelloWorld/public/js/helloworld.js
+++ sample/HelloWorld/public/js/helloworld.js
@@ -7,7 +7,9 @@ var publisher = OT.initPublisher('publisher', {
     width: '100%',
     height: '100%'
 }, function(error) {
-  alert(error);
+  if (error) {
+    console.error('Failed to initialise publisher', error);
+  }
 });
 
 // Attach event handlers
@@ -18,7 +20,9 @@ session.on({
     // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
     // clients)
     session.publish(publisher, function(error) {
-      alert(error);
+      if (error) {
+        console.error('Failed to publish', error);
+      }
     });
   },
 
@@ -31,10 +35,18 @@ session.on({
     document.getElementById('subscribers').appendChild(subContainer);
 
     // Subscribe to the stream that caused this event, put it inside the container we just made
-    session.subscribe(event.stream, subContainer);
+    session.subscribe(event.stream, subContainer, function(error) {
+      if (error) {
+        console.error('Failed to subscribe', error);
+      }
+    });
   }
 
 });
 
 // Connect to the Session using a 'token'
-session.connect(token);
+session.connect(token, function(error) {
+  if (error) {
+    console.error('Failed to connect', error);
+  }
+});
