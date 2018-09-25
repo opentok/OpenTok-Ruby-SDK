5d2856d62e8acf485d01e9593b38736cf07d66e7
Jaideep Shah <jaideep@tokbox.com>
2018-09-24 17:57:26 -0700

    refactor method names, added StreamList

--- lib/opentok/streams.rb
+++ lib/opentok/streams.rb
@@ -27,11 +27,33 @@ module OpenTok
     # @raise [OpenTokAuthenticationError] You are not authorized to fetch the stream information. Check your authentication credentials.
     # @raise [OpenTokError] An OpenTok server error.
     #
-    def info_stream(session_id, stream_id = '')
+    def this_one(session_id, stream_id)
       raise ArgumentError, 'session_id not provided' if session_id.to_s.empty?
+      raise ArgumentError, 'stream_id not provided' if session_id.to_s.empty?
       stream_json = @client.info_stream(session_id, stream_id)
       Stream.new stream_json
     end
+    # Use this method to get information on all OpenTok stream.
+    #
+    # For example, you can call this method to get information about layout classes used by an OpenTok stream.
+    # The layout classes define how the stream is displayed in the layout of a broadcast stream.
+    # For more information, see Assigning{https://tokbox.com/developer/guides/broadcast/live-streaming/#assign-layout-classes-to-streams}
+    # live streaming layout classes to OpenTok streams.
+    #
 
+    # For more information on getting stream information, see the
+    # {https://tokbox.com/developer/rest/#get-stream-info} programming guide.
+    #
+    # @param [String] session_id The session ID of the OpenTok session.
+    # @return [StreamList] The StreamList of Stream objects
+    # @raise [ArgumentError] stream_id or session_id is invalid.
+    # @raise [OpenTokAuthenticationError] You are not authorized to fetch the stream information. Check your authentication credentials.
+    # @raise [OpenTokError] An OpenTok server error.
+    #
+    def all(session_id)
+      raise ArgumentError, 'session_id not provided' if session_id.to_s.empty?
+      response_json = @client.info_stream(session_id, '')
+      StreamList.new response_json
+    end
   end
 end
--- spec/opentok/streams_spec.rb
+++ spec/opentok/streams_spec.rb
@@ -2,6 +2,7 @@ require 'opentok/opentok'
 require 'opentok/version'
 require 'opentok/streams'
 require 'opentok/stream'
+require 'opentok/stream_list'
 
 require 'spec_helper'
 
@@ -23,22 +24,25 @@ describe OpenTok::Streams do
   it { should be_an_instance_of OpenTok::Streams }
   it 'raise an error on nil session_id' do
     expect {
-      streams.info_stream(nil)
+      streams.all(nil)
     }.to raise_error(ArgumentError)
   end
   it 'raise an error on empty session_id' do
     expect {
-      streams.info_stream('')
+      streams.all('')
     }.to raise_error(ArgumentError)
   end
   it 'get all streams information', :vcr => { :erb => { :version => OpenTok::VERSION + "-Ruby-Version-#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}"} } do
-    response = streams.info_stream(session_id)
-    expect(response).not_to be_nil
+    all_streams = streams.all(session_id)
+    expect(all_streams).to be_an_instance_of OpenTok::StreamList
+    expect(all_streams.total).to eq 2
   end
   it 'get specific stream information', :vcr => { :erb => { :version => OpenTok::VERSION + "-Ruby-Version-#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}"} } do
-    stream = streams.info_stream(session_id, stream_id)
+    stream = streams.this_one(session_id, stream_id)
     expect(stream).to be_an_instance_of OpenTok::Stream
     expect(stream.videoType).to eq 'camera'
+    expect(stream.layoutClassList.count).to eq 1
+    expect(stream.layoutClassList.first).to eq "full"
     expect(stream.id).not_to be_nil
   end
 end
