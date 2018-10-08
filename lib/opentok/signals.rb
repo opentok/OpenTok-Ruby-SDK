04dd6d1f190de025f30d460b38ff5f55d20e8db1
Jeff Swartz <swartz@tokbox.com>
2018-10-08 16:39:37 -0700

    Docs corrections.

--- lib/opentok/signals.rb
+++ lib/opentok/signals.rb
@@ -15,19 +15,25 @@ module OpenTok
     # {https://tokbox.com/developer/rest/#send_signal}.
     #
     # @param [String] session_id The session ID of the OpenTok session.
-    # @param [Hash] options  A hash with the keys 'type' and 'data'.
+    #
     # @option options [String] :type This is the type of the signal. You can use this
     # field to group and filter signals. It is a property of the Signal object received by
     # the client(s).
     # @option options [String] :data This is the data within the signal or the payload.
-    # Contains main information to be sent in the signal. It is a property of the Signal object
+    # Contains the main information to be sent in the signal. It is a property of the Signal object
     # received by the client(s).
-    # @option options [String] :connId When a connId is specified only that connection recieves a signal. Otherwise, the signal is sent to all clients connected to the session.
+    # @option options [String] :connection_id When a connId is specified, only that connection
+    #   recieves a signal. Otherwise, the signal is sent to all clients connected to the session.
     #
-    # @raise [OpenTokSignalingError] Signaling failed due to invalid properties.
-    # @raise [OpenTokSignalingError] You are not authorized to send the signal. Check your authentication credentials.
-    # @raise [OpenTokArchiveError] The client specified by the connId property is not connected to the session.
-    # @raise [OpenTokArchiveError] The type string exceeds the maximum length (128 bytes), or the data string exceeds the maximum size (8 kB).
+    # @raise [ArgumentError]
+    #   One of the signal properties — data, type, session_id, or connection_id — is invalid.
+    # @raise [OpenTokAuthenticationError]
+    #   You are not authorized to send the signal. Check your authentication credentials.
+    # @raise [OpenTokError]
+    #   The client specified by the connId property is not connected to the session.
+    # @raise [OpenTokError]
+    #   The type string exceeds the maximum length (128 bytes), or the data string exceeds
+    #   the maximum size (8 kB).
     def send(session_id, connectionId = "", options = {})
       raise ArgumentError, "session_id not provided" if session_id.to_s.empty?
       response = @client.signal(session_id, connectionId, options)
