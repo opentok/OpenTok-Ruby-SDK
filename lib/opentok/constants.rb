43acf8ea773bf56c7496a1b2191482227cfa0971
benhayehudi <bengreenberg@gmail.com>
2020-02-25 14:55:23 +0200

    Fixed Set to be called correctly from Constants class

--- lib/opentok/constants.rb
+++ lib/opentok/constants.rb
@@ -2,6 +2,6 @@ module OpenTok
   API_URL = "https://api.opentok.com"
   TOKEN_SENTINEL = "T1=="
   ROLES = { subscriber: "subscriber", publisher: "publisher", moderator: "moderator" }
-  ARCHIVE_MODES = Set.new([:manual, :always])
+  ARCHIVE_MODES = ::Set.new([:manual, :always])
   AUTH_EXPIRE = 300
 end
