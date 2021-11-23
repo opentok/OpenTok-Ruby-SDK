753c851324ef66e94dc26d204d6b0479a723c2e6
superchilled <karl@superchilled.co.uk>
2021-11-23 19:43:40 +0000

    Adding Archives#add_stream method

--- lib/opentok/archives.rb
+++ lib/opentok/archives.rb
@@ -50,8 +50,8 @@ module OpenTok
     #   results in an error.
     # @option options [Hash] :layout Specify this to assign the initial layout type for
     #   the archive. This applies only to composed archives. This is a hash containing three keys:
-    #   <code>:type</code>, <code>:stylesheet<code> and <code>:screenshare_type</code>. 
-    #   Valid values for <code>:type</code> are "bestFit" (best fit), "custom" (custom), 
+    #   <code>:type</code>, <code>:stylesheet<code> and <code>:screenshare_type</code>.
+    #   Valid values for <code>:type</code> are "bestFit" (best fit), "custom" (custom),
     #    "horizontalPresentation" (horizontal presentation),
     #   "pip" (picture-in-picture), and "verticalPresentation" (vertical presentation)).
     #   If you specify a "custom" layout type, set the <code>:stylesheet</code> key to the
@@ -171,10 +171,10 @@ module OpenTok
     # {https://tokbox.com/developer/guides/archiving/layout-control.html Customizing
     # the video layout for composed archives}.
     #
-    # @param [String] archive_id 
+    # @param [String] archive_id
     #   The archive ID.
     #
-    # @option options [String] :type 
+    # @option options [String] :type
     #   The layout type. Set this to "bestFit", "pip", "verticalPresentation",
     #   "horizontalPresentation", "focus", or "custom".
     #
@@ -226,5 +226,33 @@ module OpenTok
       response = @client.layout_archive(archive_id, options)
       (200..300).include? response.code
     end
+
+    def add_stream(archive_id, stream_mode, options)
+      raise ArgumentError, "archive_id not provided" if archive_id.to_s.empty?
+      raise ArgumentError, "stream_mode must be manual in order to add a stream" unless stream_mode == 'manual'
+      raise ArgumentError, "option parameter is empty" if options.empty?
+      add_stream = options[:add_stream]
+      raise ArgumentError, "add_stream not provided" if add_stream.to_s.empty?
+      has_audio = options[:has_audio]
+      raise ArgumentError, "has_audio not provided" if has_audio.to_s.empty?
+      raise ArgumentError, "has_audio must be true or false" unless is_boolean?(has_audio)
+      has_video = options[:has_video]
+      raise ArgumentError, "has_video not provided" if has_video.to_s.empty?
+      raise ArgumentError, "has_video must be true or false" unless is_boolean?(has_video)
+      raise ArgumentError, "at least one of has_audio and has_video must be true" unless audio_and_video_options_valid?(has_audio, has_video)
+
+      @client.select_streams_for_broadcast(broadcast_id, options)
+    end
+
+    private
+
+    def is_boolean?(value)
+      [true, false].include? value
+    end
+
+    def audio_and_video_options_valid?(has_audio, has_video)
+      has_audio == true || has_video == true
+    end
+    
   end
 end
